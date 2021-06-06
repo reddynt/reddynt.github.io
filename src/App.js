f86788400c141f951e23256639183c44b86a3640
TN <tharun@TNs-MacBook-Air.local>
2021-06-06 09:40:20 +0530

    website init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        Portfolio webiste: tharun
+      </header>
+    </div>
+  );
+}
+
+export default App;
